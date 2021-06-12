a57005817d68ae47c52a11222d66f7f0d6215194
webmiraclepro <webmiraclepro@gmail.com>
2021-06-12 20:40:30 +0800

    load data to django model

--- backend/views/models.py
+++ backend/views/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Currency(models.Model):
     t = models.IntegerField(blank=False)
-    prediction = models.RealField(blank=True, null=True)
-    realVal = models.RealField(blank=True, null=True)
+    prediction = models.FloatField(blank=True, null=True)
+    realVal = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return self.name
