bf2266139a21e8cb743dc0b6d76f28be21706855
webmiraclepro <webmiraclepro@gmail.com>
2021-06-13 15:16:51 +0800

    update django currency model

--- backend/views/serializers.py
+++ backend/views/serializers.py
@@ -4,5 +4,6 @@ from .models import Currency
 class CurrencySerializer(serializers.ModelSerializer):
 
     class Meta:
+        db_table = 'currency'
         model = Currency 
         fields = ('pk', 't', 'prediction', 'realVal')
