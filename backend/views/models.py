8c383957669eefd87d7ce07118165fee3b7dbd34
webmiraclepro <webmiraclepro@gmail.com>
2021-06-13 15:39:40 +0800

    update django currency model

--- backend/views/models.py
+++ backend/views/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Currency(models.Model):
-    t = models.IntegerField(blank=False)
+    t = models.IntegerField(blank=False, primary_key=True)
     prediction = models.FloatField(blank=True, null=True)
     realVal = models.FloatField(blank=True, null=True)
 
